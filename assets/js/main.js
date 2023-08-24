ee7efc6b7040392801bb0ec1f15a00fcab23ec3d
Ruan Souza <ruan2014souza17@gmail.com>
2023-08-24 09:26:03 -0300

    adicionando pokemons, tipos, fotos e nomes dinamicamente

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,28 @@
-console.log('sucesso')
+
+
+
+function convertPokemonToLi(pokemon) {
+    return `
+    <li class="pokemon">
+    <span class="number">#${pokemon.number}</span>
+    <span class="name">${pokemon.name}</span>
+   
+    <div class="detail">
+        <ol class="types">
+            ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+        </ol>
+    <img src="${pokemon.photo}" 
+    alt="${pokemon.name}</">
+</div>
+</li>
+`
+}
+
+const pokekmonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons) => {
+   const newHtml = pokemons.map((convertPokemonToLi)).join('')
+    pokekmonList.innerHTML = newHtml
+    
+})
+
